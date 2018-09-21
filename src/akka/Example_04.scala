ffce2039bc3c212151b31d348493be61c719ff79
hjd <hjd1622@qq.com>
2018-09-21 18:02:44 +0800

    0.5.1 增加akka 学习的例子  补充context.actorOf的使用

---
+++ src/akka/Example_04.scala
@@ -0,0 +1,48 @@
+package akka
+
+import akka.actor.ActorLogging
+
+/**
+  * Created by Administrator on 2018/9/20.
+  */
+object Example_04 extends App{
+  import akka.actor.Actor
+  import akka.actor.ActorSystem
+  import akka.actor.Props
+
+
+  class FirstActor extends Actor with ActorLogging{
+    //通过context.actorOf方法创建Actor
+    /**
+      * 也就是说context.actorOf和system.actorOf的差别是system.actorOf创建的actor为顶级Actor，
+      * 而context.actorOf方法创建的actor为调用该方法的Actor的子Actor
+      */
+    val child = context.actorOf(Props[MyActor], name = "myChild") //MyActor是FirstActor 的子Actor
+    def receive = {
+      case x => child ! x
+        log.info("received "+x)
+    }
+
+  }
+
+  class MyActor extends Actor with ActorLogging{
+    def receive = {
+      case "test" => log.info("received test")
+      case _      => log.info("received unknown message")
+    }
+  }
+
+  val system = ActorSystem("MyActorSystem")
+  val systemLog=system.log
+
+  //创建FirstActor对象
+  val myactor = system.actorOf(Props[FirstActor], name = "firstActor")
+
+  systemLog.info("准备向myactor发送消息")
+  //向myactor发送消息
+  myactor!"test"
+  myactor! 123
+  Thread.sleep(5000)
+  //关闭ActorSystem，停止程序的运行
+  system.shutdown()
+}
--- src/collection/IcFk1.scala
+++ src/collection/IcFk1.scala
@@ -1,35 +1,28 @@
-//package com.hxtc.hsm
-//
-//import com.hxtc.hsm.service.SynchroService
-//import net.liftweb.json.{JObject, parse}
-//
-//import scala.concurrent.ExecutionContext.Implicits.global
-//import scala.concurrent.Future
-//
-//object IcFk1 extends App {
-//  val threads = 100
-//  val times = 5000
-//  val start = System.currentTimeMillis()
-//  println(s"start: $start")
-//  for (j <- 1 to threads) {
-//    val f = Future {
-//      for (i <- 1 to times) {
-//        //          val req = s"""14ICC_gainICCard2{"kek1":"IC-ZMK-X","kek":"IC-ZEK-X","cardBin":"000002","certSN" :"000002","keyMode":1024,"ef04":"1234567890123457","ef06":"1234567890123457","ef08":"1234567890123457","ef10":"1234567890123457","ef22":"1234567890123457","ef23":"1234567890123457","iccPAN":"62297568345678901234","t9301":"5F24,5A,9F07,8E,9F0D,9F0E,9F0F,5F28,82","v9301":"5F24031911305A0A6223220000000094FFFF5F3401009F0702FF008E0C000000000000000002031F009F0D05D86004A8009F0E0500109800009F0F05D86804F8009F280201567C00","iccFormat01":"04","iccExpires01":"1219","checkCode01":"0000","t9302":"5F24,5A,9F07,8E,9F0D,9F0E,9F0F,5F28,82","v9302":"5F24031911305A0A6223220000000094FFFF5F3401009F0702FF008E0C000000000000000002031F009F0D05D86004A8009F0E0500109800009F0F05D86804F8009F280201567C00","iccFormat02":"04","iccExpires02":"1219","checkCode02":"0000","t9303":"","v9303":"","iccFormat03":"","iccExpires03":"","checkCode03":"","iccFormatPine":"","iccExpiresPine":"","checkCodePine":"","specs":"pboc"} """
-////        val req =
-////          s"""09KMC_010012{"BMK":"BSM-X-001","Index":"BSM-001-ZPK-X"}"""
-//               val req =s"""02NC2{}"""
-//        val res = parse(SynchroService.connHost("127.0.0.1", 21201, "07console", req)).asInstanceOf[JObject]
-//      }
-//    }
-//    f.onSuccess {
-//      case _ =>
-//        val end = System.currentTimeMillis()
-//        //        println(s"Thread $j - Run Time ${end - start}ms")
+package com.hxtc.hsm
+
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+object IcFk1 extends App {
+  val threads = 1
+  val times = 5000
+  val start = System.currentTimeMillis()
+  println(s"start: $start")
+  for (j <- 1 to threads) {
+    val f = Future {
+      for (i <- 1 to times) {
+       println(Thread.currentThread().getName)
+      }
+    }
+    f.onSuccess {
+      case _ =>
+        val end = System.currentTimeMillis()
 //        println(s"Thread $j - TPS ${times * 1000 / (end - start)}")
-//    }
-//
-//  }
-//
-//  Thread.sleep(10000000)
-//
-//}
+    }
+
+  }
+
+  Thread.sleep(10000000)
+
+}
